451edcb5222fe3940e7fda2fc7123547346eb87c
niko.sarcevic <nikolina.sarcevic@gmail.com>
2023-09-05 14:16:57 +0100

    code cleanup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import os
 import warnings
 import streamlit as st
+import tempfile
+import pandas as pd
 from datetime import datetime
 from helpers import create_download_link, figure_to_base64, process_files
+from gca_streamlit import GeneCocktailAnalyser
 
 def setup_page():
     st.set_page_config(layout="wide")
@@ -25,51 +28,57 @@ if st.sidebar.button("Analysis"):
     uploaded_cocktail, uploaded_filters = upload_files()
 
     if uploaded_cocktail and uploaded_filters:
-        analyser, temp_cocktail, temp_filters = process_files(uploaded_cocktail, uploaded_filters)
-        st.write("Uploaded files processed!")
 
-        # Generate the report using the display_results method
-        report_lines = analyser.display_results()
-        output_str = "\n".join(report_lines)
-        filename_input = st.text_input("Enter name for the report:", "")
-        filename = filename_input if filename_input else f"report_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            # Set the current working directory to the temp directory
+            os.chdir(tmpdirname)
 
-        # Button to download the report as a TXT file
-        if st.button("Download Report as TXT"):
-            link = create_download_link(output_str, filename, "Click here to download the report")
-            st.markdown(link, unsafe_allow_html=True)
+            cocktail_df = pd.read_csv(uploaded_cocktail)
+            filters_df = pd.read_csv(uploaded_filters)
 
-        # Another button to unveil/display the report on the Streamlit app
-        if st.button("Display Report"):
-            st.text(output_str)
+            # Write cocktail_df to a temporary CSV
+            temp_cocktail = tempfile.NamedTemporaryFile(delete=False, suffix='.csv')
+            cocktail_df.to_csv(temp_cocktail.name, index=False)
 
-        if st.button("Plot Visualizations"):
-            st.write("Plotting visualizations...")
-            st.write("Plotting visualizations...")
-            fig1 = analyser.plot_summary_data()
-            st.pyplot(fig1, width=400, height=400)
-            encoded_fig1 = figure_to_base64(fig1)
-            plot1_filename = f"{filename_input}_summary_plot.png" if filename_input else "summary_plot.png"
-            st.markdown(
-                f'<a href="data:image/png;base64,{encoded_fig1}" download="{plot1_filename}">Download Summary Plot</a>',
-                unsafe_allow_html=True)
+            # Write filters_df to a temporary CSV
+            temp_filters = tempfile.NamedTemporaryFile(delete=False, suffix='.csv')
+            filters_df.to_csv(temp_filters.name, index=False)
 
-            fig2 = analyser.plot_frequency_of_matches()
-            st.pyplot(fig2, width=400, height=400)
-            encoded_fig2 = figure_to_base64(fig2)
-            plot2_filename = f"{filename_input}_histogram.png" if filename_input else "histogram.png"
-            st.markdown(
-                f'<a href="data:image/png;base64,{encoded_fig2}" download="{plot2_filename}">Download Histogram Plot</a>',
-                unsafe_allow_html=True)
+            analyser = GeneCocktailAnalyser(temp_cocktail.name, temp_filters.name)
+            analyser.process_data()
+            st.write("Uploaded files processed!")
 
-            fig3 = analyser.plot_heatmap()
-            st.pyplot(fig3, width=400, height=400)
-            encoded_fig3 = figure_to_base64(fig3)
-            plot3_filename = f"{filename_input}_heatmap.png" if filename_input else "heatmap.png"
-            st.markdown(
-                f'<a href="data:image/png;base64,{encoded_fig3}" download="{plot3_filename}">Download Heatmap Plot</a>',
-                unsafe_allow_html=True)
+            # Allow user to provide a custom name for the report
+            filename_input = st.text_input("Enter name for the report:", "")
 
+            # If the user hasn't provided a name, generate a default one based on the current date and time
+            filename = filename_input if filename_input else f"report_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+
+            if st.button("Download Report as TXT"):
+                # Generate the report using the display_results method
+                report_lines = analyser.display_results()
+                output_str = "\n".join(report_lines)
+                link = create_download_link(output_str, filename, "Click here to download the report")
+                st.markdown(link, unsafe_allow_html=True)
+
+            if st.button("Display Report"):
+                report_lines = analyser.display_results()
+                output_str = "\n".join(report_lines)
+                st.text(output_str)
+
+            if st.button("Plot Visualizations"):
+                st.write("Plotting visualizations...")
+
+                fig1 = analyser.plot_summary_data()
+                st.pyplot(fig1, width=400, height=400)
+
+                fig2 = analyser.plot_frequency_of_matches()
+                st.pyplot(fig2, width=400, height=400)
+
+                fig3 = analyser.plot_heatmap()
+                st.pyplot(fig3, width=400, height=400)
+
+            # Cleanup temporary files
             os.remove(temp_cocktail.name)
             os.remove(temp_filters.name)
 
@@ -77,6 +86,10 @@ if st.sidebar.button("Analysis"):
 if st.sidebar.button("Intro"):
     st.write("This is the introduction to the Gene Cocktail Analyser.")
 
+
 # About
 if st.sidebar.button("About"):
     st.write("Information about the Gene Cocktail Analyser and its purpose.")
+
+    st.write("Made by Niko Sarcevic, Newcastle University")
+    st.write("Visit www.nikosarcevic.com or github.com/nikosarcevic")
